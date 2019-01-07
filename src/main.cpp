e06bb99f252ed6750696b4bc50c9d7e30523b0d2
Robert Fox <fox.robert92@gmail.com>
2019-01-07 12:34:54 -0800

    added initial docopts

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
-#include "docopt.h"
+#include "docopt.cpp"
 
 
+static const char USAGE[] =
+R"(
+    Graph Fixturing.
+    Uses maximally weighted matching to provide a fixture for a league
+    Supports Google Sheets-stored scores, ladders and previous fixtures
 
-int main()
+    Usage:
+        gFixture leagueName <name> sheet <url>
+
+    Options:
+        -h  --help                      Show this screen
+        --rematchCount=<rematchCount>   Allowed rematch count [default: 0]
+        --round=<roundNumber>           Round Number to apply to the output file
+)";
+
+
+int main(int argc, const char** argv)
 {
-    std::cout << "hello, world!\n";
+    std::map<std::string, docopt::value> args
+        = docopt::docopt(USAGE,
+                         { argv + 1, argv + argc },
+                         true,               // show help if requested
+                         "gFixture 0.1");  // version string
+
+    for(auto const& arg : args) {
+        std::cout << arg.first << " : "<<  arg.second << std::endl;
+    }
+
     return 0;
     
 }
